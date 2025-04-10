7fa28a5c5494ccf2a69f879632c0c0e8ba58b483
Angel Ramirez <angelgabriel.16ra@gmail.com>
2025-04-10 16:22:17 -0600

    Starting Lesson 03

---
+++ lesson03/CrudApp/data/todos.js
@@ -0,0 +1,52 @@
+export const data =[
+    {
+        'id':1,
+        "title": "Learn React Native",
+        "completed": false,
+    },
+    {
+        'id':2,
+        "title": "Learn Expo",
+        "completed": false,
+    },
+    {
+        'id':3,
+        "title": "Learn Redux",
+        "completed": false,
+    },
+    {
+        'id':4,
+        "title": "Write a book",
+        "completed": false,
+    },
+    {
+        'id':5,
+        "title": "Buy groceries",
+        "completed": true,
+    },
+    {
+        'id':6,
+        "title": "Go to the gym",
+        "completed": false,
+    },
+    {
+        'id':7,
+        "title": "Finish reading the book",
+        "completed": true,
+    },
+    {
+        'id':8,
+        "title": "Go to the store",
+        "completed": false,
+    },
+    {
+        'id':9,
+        "title": "Clean the house",
+        "completed": true,
+    },
+    {
+        'id':10,
+        "title": "Call the bank",
+        "completed": false,
+    }
+]
